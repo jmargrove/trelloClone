44d2517e3b336660744df38dfe855927be184831
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2018-01-14 21:15:55 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Header from "containers/Header.js";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/Header.js
@@ -0,0 +1,9 @@
+import react, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return <div className="headerBanner">hello</div>;
+  }
+}
+
+export default Header;
