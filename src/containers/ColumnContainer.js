a0724588f45b7bf58ffdbfcef0ec5406f64baf52
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2018-01-22 22:31:51 +0100

    fix: two maps have keys uuid

--- src/containers/ColumnContainer.js
+++ src/containers/ColumnContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ColumnList from "./ColumnList.js";
 import AddList from "./AddList.js";
 import { connect } from "react-redux";
+import uuid from "uuid/v1";
 
 const mapStateToProps = state => ({
   listTitles: state.listTitles
@@ -12,7 +13,9 @@ class ColumnContainer extends Component {
     console.log("this.state", this.props);
     return (
       <div className="ColumnContainer">
-        {this.props.listTitles.map(title => <ColumnList listTitle={title} />)}
+        {this.props.listTitles.map(title => (
+          <ColumnList key={uuid()} listTitle={title} />
+        ))}
         <AddList />
       </div>
     );
