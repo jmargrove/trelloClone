dcbcff1a7aae6138a3f75e53d6f80cea1a35efe3
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2018-02-03 19:13:51 +0100

    feat: dnd is working for cols is working

--- src/App.js
+++ src/App.js
@@ -2,24 +2,17 @@ import React, { Component } from "react";
 import Header from "./containers/Header.js";
 import ProjectTitleBar from "./containers/ProjectTitleBar.js";
 import ColumnContainer from "./containers/ColumnContainer.js";
-import { DragDropContext } from "react-beautiful-dnd";
+
 import "./App.css";
 
 class App extends Component {
-  onDragStart = props => {};
-  onDragEnd = props => {};
   render() {
     return (
-      <DragDropContext
-        onDragStart={this.onDragStart}
-        onDragEnd={this.onDragEnd}
-      >
-        <div className="App">
-          <Header />
-          <ProjectTitleBar />
-          <ColumnContainer />
-        </div>
-      </DragDropContext>
+      <div className="App">
+        <Header />
+        <ProjectTitleBar />
+        <ColumnContainer />
+      </div>
     );
   }
 }
